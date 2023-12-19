6f5bef518b058873e8725c7721ae4c474264a673
siam <sayemuzzaman505@gmail.com>
2023-12-19 18:53:58 +0600

    adding include fol from alpha

---
+++ include/connect.php
@@ -0,0 +1,11 @@
+
+<?php
+$conn = mysqli_connect('localhost','root','','mystore');
+
+if($conn){
+  
+}else{
+  die(mysqli_error($conn));
+}
+
+?>
---
+++ include/footer.php
@@ -0,0 +1,31 @@
+
+<!-- Bootstrap Css Link -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous">
+
+    <!-- Font Awesome Link -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer">
+
+<!-- Last Child -->
+<div class="bg-dark p-2 text-center h-20 mt-5">
+<h1 class ="text-white"> About Us </h1><br>
+<p class="text-white"> We are the biggest Bike showroom in Bangladesh </p>
+<p class="text-white"> To contact with us: bikemonster@gmail.com </p>
+
+
+
+</div> 
+
+
+
+
+<!-- Boostrap JS link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
